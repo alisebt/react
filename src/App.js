2588ec3d2776b367fb4d11eaafc4188ee9f728de
Ali Sebt <alisebt@gmail.com>
2024-04-30 14:00:56 +0330

    -add API to the application

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProductList from "./components/ProductList/ProductList";
 import AddProduct from "./components/AddProduct/AddProduct";
 
 const App = () => {
-    const [products, setProducts] = useState([
-        { id: 1, title: "samsung" },
-        { id: 2, title: "apple" },
-        { id: 3, title: "xiaomi" }
-    ]);
+    const [products, setProducts] = useState([]);
 
-    const deleteProduct = (id) => {
-        setProducts(products.filter((item) => item.id !== id ));
+    useEffect(() => {
+        const sendRequest = async () => {
+            const response = await fetch('http://localhost:8000/products');
+            const responseData = await response.json();
+
+            setProducts(responseData);
+        }
+
+        sendRequest();
+    }, []);
+
+    const deleteProduct = async (id) => {
+        await fetch(`http://localhost:8000/products/${id}`, {
+            method: 'DELETE'
+        });
+
+        setProducts(products.filter((item) => item.id !== id));
+    }
+
+    const addProduct = async (title) => {
+        const id = Math.floor(Math.random() * 10000);
+
+        const newProduct = { id, ...title };
+
+        const response =  await fetch(`http://localhost:8000/products`, {
+            method: 'POST',
+            headers:{
+                'Content-type':'application/json'
+            },
+            body: JSON.stringify(title),
+        });
+
+        const responseData = await response.json();
+
+
+        setProducts([...products, responseData]);
     }
 
     return <div className="container">
-        <AddProduct />
+        <AddProduct onAdd={addProduct} />
         <ProductList products={products} onDelete={deleteProduct} />
     </div>;
 }
--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './AddProduct.css'
 
-const AddProduct = () => {
+const AddProduct = ({ onAdd }) => {
+    const [title, setTitle] = useState('');
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        onAdd({ title });
+
+        setTitle('');
+    }
+
     return (
         <div>
-            <form className='add-product-form'>
+            <form className='add-product-form' onSubmit={submitForm}>
                 <div className='form-control'>
-                    <input type='text' placeholder='Title'></input>
+                    <input type='text' placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)}></input>
                 </div>
                 <button type='submit' className='form-btn'>Add</button>
             </form>
